be063cc05708db365ce296419c1216b8440a9d56
Hemant Kumar <hemantkumarcpersonal@gmail.com>
2024-04-04 19:15:39 +0530

    Fix Navbar related issue and add route for admin dashboard

--- React-Server/src/App.js
+++ React-Server/src/App.js
@@ -5,6 +5,8 @@ import PatientLogin from "./pages/patients/Login";
 import PatientDialer from "./pages/patientDialer/PatientDialer";
 import CounsellorDashboard from "./pages/counsellorDashboard/CounsellorDashboard";
 import InCall from "./pages/inCall/InCall";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminCreateCounsellor from "./pages/admin/AdminCreateCounsellor";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -16,6 +18,11 @@ function App() {
         <Route path="/patientdialer" element={<PatientDialer />} />
         <Route path="/counsellorDashboard" element={<CounsellorDashboard />} />
         <Route path="/inCall" element={<InCall />} />
+        <Route path="/adminDashboard" element={<AdminDashboard />} />
+        <Route
+          path="/adminCreateCounsellor"
+          element={<AdminCreateCounsellor />}
+        />
       </Routes>
     </BrowserRouter>
   );
