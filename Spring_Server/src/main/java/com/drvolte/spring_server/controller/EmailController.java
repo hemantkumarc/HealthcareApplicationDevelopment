ec2d4c9966bc4758cf8b6cc4cb6f73147d1c9def
Hemant Kumar <hemantkumarcpersonal@gmail.com>
2024-04-12 12:38:10 +0530

    Change email controller

--- Spring_Server/src/main/java/com/drvolte/spring_server/controller/EmailController.java
+++ Spring_Server/src/main/java/com/drvolte/spring_server/controller/EmailController.java
@@ -12,10 +12,26 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
+
+    // Please add method level security so that only ROLE_ADMIN is authorized to use this endpoint
     @PostMapping("/send/{mailId}")
-    public String sendMail(@PathVariable String mailId, @RequestBody EmailStructure emailStructure){
+    public String sendMail(@PathVariable String mailId){
+        EmailStructure emailStructure = new EmailStructure();
+        emailStructure.setSubject("[Dr. Volte] Register Doctor");
+        emailStructure.setMessage("Please click on the following link to create password and register yourself !");
         emailService.sendMail(mailId, emailStructure);
         return "Successfully sent the mail !!";
     }
+
+//    @PostMapping("/send/{mailId}")
+//    public String sendMail(@PathVariable String mailId, @RequestBody EmailStructure emailStructure){
+//        emailService.sendMail(mailId, emailStructure);
+//        return "Successfully sent the mail !!";
+//    }
+
+
+    // Please add another method for forgot password that will trigger the usage of email service which
+    // will check if the given user exists in the database.
+    // Also add method level security to this.
 
 }
