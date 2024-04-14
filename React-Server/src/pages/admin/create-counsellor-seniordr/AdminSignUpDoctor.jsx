93bda3d3d4a33d4fdf6ba5dd36a7160634a35a47
Hemant Kumar <hemantkumarcpersonal@gmail.com>
2024-04-14 13:04:55 +0530

    Remove comments

--- React-Server/src/pages/admin/create-counsellor-seniordr/AdminSignUpDoctor.jsx
+++ React-Server/src/pages/admin/create-counsellor-seniordr/AdminSignUpDoctor.jsx
@@ -65,60 +65,6 @@ const AdminSignUpDoctor = () => {
       toast.error("Passwords arent't matching !");
     }
     console.log("Form submitted !");
-
-    // const finalFormData = {
-    //   ...formData,
-    //   languages: JSON.stringify(updatedLanguage),
-    //   status: formData.status.toString() === "true" ? "enabled" : "disabled",
-    // };
-
-    // const doctors_email_response = await api.get(
-    //   GET_DOCTORS_BY_EMAIL + `${finalFormData.email}`
-    // );
-
-    // const SEND_MAIL_ENDPOINT = SEND_MAIL + `${finalFormData.email}`;
-
-    // const check = doctors_email_response?.data?._embedded?.doctors;
-    // if (check !== undefined && check?.length === 0) {
-    //   // Now we can either check if he wants to be senior doctor or not
-
-    //   if (finalFormData.isSeniorDoctor) {
-    //     delete finalFormData.isSeniorDoctor;
-    //     try {
-    //       const response = await api.post(
-    //         SAVE_SENIORDR_ENDPOINT,
-    //         JSON.stringify(finalFormData),
-    //         {
-    //           headers: { "Content-Type": "application/json" },
-    //         }
-    //       );
-    //       console.log(response?.status);
-    //     } catch (err) {}
-    //     toast.success(
-    //       "The Senior Doctor is sent a mail and is successfully added to database !"
-    //     );
-    //   } else {
-    //     delete finalFormData.isSeniorDoctor;
-    //     try {
-    //       const response = await api.post(
-    //         SAVE_COUNSELLOR_ENDPOINT,
-    //         JSON.stringify(finalFormData),
-    //         {
-    //           headers: { "Content-Type": "application/json" },
-    //         }
-    //       );
-    //       console.log(response?.status);
-    //     } catch (err) {}
-    //     toast.success(
-    //       "The Counsellor is sent a mail and is successfully added to database !"
-    //     );
-    //   }
-    // } else {
-    //   // This means that the doctor is already present
-    //   toast.error("There is already another doctor with the same email !");
-    // }
-    // console.log(JSON.stringify(finalFormData));
-    // console.log("Form Submitted");
   }
 
   return (
