d78a7d9ae0b5f1e9535329218a5fd5973a2d0618
somesh-awasthi <Somesh.Awasthi@iiitb.ac.in>
2024-04-19 11:24:21 +0530

    filter section updated ,  counsellor fixed

--- React-Server/src/pages/srDocDashboard/component/Cards.js
+++ React-Server/src/pages/srDocDashboard/component/Cards.js
@@ -6,7 +6,10 @@ export default function Cards({ counsellor }) {
     <div className="containers">
       <div className="card_item">
         <div className="card_inner">
-          <img src={counsellor.profile_photo} alt="counsellor" />
+          {/* <img src={counsellor.profile_photo} alt="counsellor" /> */}
+          <img
+            src="https://media.istockphoto.com/id/525882213/vector/crazy-doctor.jpg?s=612x612&w=0&k=20&c=2dapPSHBjpiuPdCTyrJBk6YD_k8Hlwp9SD-BJOOeius="
+            alt="counsellor"/>
           <div className="name">{counsellor.name}</div>
           <div className="qualification">{counsellor.qualification}</div>
           <div className="hospital_name">{counsellor.hospital_name}</div>
--- React-Server/src/pages/srDocDashboard/component/Counsellor.js
+++ React-Server/src/pages/srDocDashboard/component/Counsellor.js
@@ -1,117 +1,103 @@
 import React, { useEffect, useState } from "react";
 import "../style/Counsellor.css";
 import Cards from "./Cards";
-// import { getResponseGet } from "../../../utils/utils";
-import data from "../db/data";
+import { getResponseGet } from "../../../utils/utils";
+// import data from "../db/data";
 
 // let springData;
-// export default function Counsellor({ filters, search, sorts }) {
-// 	const [isDataLoaded, setIsDataLoaded] = useState(null);
-// 	const [sortedCounsellors, setSortedCounsellors] = useState([]);
-
-	// useEffect(() => {
-	// 	console.log("i am here");
-	// 	const getData = async () => {
-	// 		springData = await getResponseGet("/springdatarest/counsellors");
-	// 		console.log("this is spring data", springData);
-	// 		if (springData?.data?._embedded?.counsellors) {
-	// 			springData = springData?.data?._embedded?.counsellors;
-	// 			console.log(springData);
-	// 			setIsDataLoaded(true);
-	// 		}
-	// 	};
-	// 	getData();
-	// }, []);
-
-	// useEffect(() => {
-	// 	if (isDataLoaded) {
-	// 		const sorted = springData
-	// 			.filter((counselor) => {
-	// 				const matchesSpecialization =
-	// 					filters.specialization.length === 0 ||
-	// 					filters.specialization.includes(counselor.specialization);
-	// 				const matchesLanguage =
-	// 					filters.language.length === 0 ||
-	// 					counselor.languages_spoken.some((language) =>
-	// 						filters.language.includes(language)
-	// 					);
-
-	// 				const matchesSearchTerm =
-	// 					search === "" ||
-	// 					counselor.name.toLowerCase().includes(search.toLowerCase());
-
-	// 				return matchesSpecialization && matchesLanguage && matchesSearchTerm;
-	// 			})
-	// 			.sort((a, b) => {
-	// 				let result = 0;
-
-	// 				if (sorts.arrangeBy === "ascending") {
-	// 					if (a[sorts.sortBy] > b[sorts.sortBy]) {
-	// 						result = 1;
-	// 					} else if (a[sorts.sortBy] < b[sorts.sortBy]) {
-	// 						result = -1;
-	// 					}
-	// 				} else {
-	// 					if (a[sorts.sortBy] < b[sorts.sortBy]) {
-	// 						result = 1;
-	// 					} else if (a[sorts.sortBy] > b[sorts.sortBy]) {
-	// 						result = -1;
-	// 					}
-	// 				}
-
-	// 				return result;
-	// 			});
-
-	// 		setSortedCounsellors(sorted);
-	// 	}
-	// }, [isDataLoaded]);
-
-	
 export default function Counsellor({ filters, search, sorts }) {
-	const sortedCounsellors = data
-	  .filter((counselor) => {
-		const matchesSpecialization =
-		  filters.specialization.length === 0 ||
-		  filters.specialization.includes(counselor.specialization);
-		const matchesLanguage =
-		  filters.language.length === 0 ||
-		  counselor.languages_spoken.some((language) =>
-			filters.language.includes(language)
-		  );
-  
-		const matchesSearchTerm =
-		  search === "" ||
-		  counselor.name.toLowerCase().includes(search.toLowerCase());
-  
-		return matchesSpecialization && matchesLanguage && matchesSearchTerm;
-	  })
-	  .sort((a, b) => {
-		let result = 0;
-  
-		if (sorts.arrangeBy === "ascending") {
-		  if (a[sorts.sortBy] > b[sorts.sortBy]) {
-			result = 1;
-		  } else if (a[sorts.sortBy] < b[sorts.sortBy]) {
-			result = -1;
-		  }
-		} else {
-		  if (a[sorts.sortBy] < b[sorts.sortBy]) {
-			result = 1;
-		  } else if (a[sorts.sortBy] > b[sorts.sortBy]) {
-			result = -1;
-		  }
-		}
-  
-		return result;
-	  });
-  
+	const [sortedCounsellors, setSortedCounsellors] = useState([]);
+	const [springData, setSpringData] = useState([]);
+	useEffect(() => {
+		console.log("i am here");
+		const fetchData = async () => {
+			try {
+				const response = await getResponseGet("/springdatarest/counsellors");
+				console.log("this is spring data", response);
+				const fetchedData = response?.data?._embedded?.counsellors;
+				if (fetchedData) {
+					setSpringData(fetchedData);
+					setSortedCounsellors(fetchedData);
+					console.log(response);
+				}
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
+		fetchData();
+	}, []);
+
+	const updateCounselorStatus = (updatedCounselor) => {
+		setSortedCounsellors((prevCounselors) =>
+			prevCounselors.map((counselor) =>
+				counselor.email === updatedCounselor.email
+					? updatedCounselor
+					: counselor
+			)
+		);
+	};
+
+	useEffect(() => {
+		const sorted = springData
+			.filter((counselor) => {
+				const matchesSpecialization =
+					filters.specialization.length === 0 ||
+					filters.specialization.includes(counselor.specialization);
+				// const matchesLanguage =
+				// 	filters.language.length === 0 ||
+				// 	counselor.languagues.some((language) =>
+				// 		filters.language.includes(language)
+				// 	);
+
+				// const matchesLanguage =
+				// 	filters.language.length === 0 ||
+				// 	filters.language.some((language) =>
+				// 		counselor.languagues.includes(language)
+				// 	);
+
+				const languages = JSON.parse(counselor.languages);
+				const matchesLanguage =
+					filters.language.length === 0 ||
+					languages.some((language) => filters.language.includes(language));
+
+				const matchesSearchTerm =
+					search === "" ||
+					counselor.name.toLowerCase().includes(search.toLowerCase());
+
+				return matchesSpecialization && matchesLanguage && matchesSearchTerm;
+			})
+			.sort((a, b) => {
+				let result = 0;
+
+				if (sorts.arrangeBy === "ascending") {
+					if (a[sorts.sortBy] > b[sorts.sortBy]) {
+						result = 1;
+					} else if (a[sorts.sortBy] < b[sorts.sortBy]) {
+						result = -1;
+					}
+				} else {
+					if (a[sorts.sortBy] < b[sorts.sortBy]) {
+						result = 1;
+					} else if (a[sorts.sortBy] > b[sorts.sortBy]) {
+						result = -1;
+					}
+				}
+
+				return result;
+			});
+
+		setSortedCounsellors(sorted);
+	}, [sortedCounsellors, filters, search, sorts]);
 
 	return (
 		<div className="counsellor-container">
 			<section className="card-container">
 				{sortedCounsellors.map((counselor) => (
 					<div key={counselor.name} className="card">
-						<Cards counsellor={counselor} />
+						<Cards
+							counsellor={counselor}
+							updateCounselorStatus={updateCounselorStatus}
+						/>
 					</div>
 				))}
 			</section>
